78edf6d2968a0a7aba2153e9343c784e189ddddb
Rhaysa Madruga <rysamad@gmail.com>
2020-10-28 19:58:51 -0300

    New board config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 import './App.css';
 
-const initialBoard = Array(9).fill('');
+const initialBoard = Array(9).fill(null);
+
+//print the boar with empty strings - OK
+// check to see if cell is empty - OK
+//when we click to set x or O
+//check winning condition
 
 function App() {
   function handleClick(index) {
     console.log(index, board[index]);
 
-    if (board[index] === '') {
+    if (board[index] === null) {
       let newBoard = [...board];
       newBoard[index] = currentPlayer;
       setBoard(newBoard);
@@ -17,35 +22,22 @@ function App() {
   }
 
   const [currentPlayer, setCurrentPlayer] = useState('X');
-  // const [initialCell, setCell] = useState('');
-
-  //print the boar with empty strings - OK
-  // check to see if cell is empty
-  //when we click to set x or O
-  //check winning condition
-
   const [board, setBoard] = useState(initialBoard);
 
   return (
     <div className='App'>
+      <p>Tic Tac Toe</p>
       <div className='board'>
         {board.map((space, index) => {
           return (
             <>
-              {index === 3 || index === 6 ? <br /> : null}
-              <div className='cel' key={index} onClick={() => handleClick(index)}>
-                | {space} |{' '}
+              {/* {index === 3 || index === 6 ? <br /> : null} */}
+              <div className='cell' key={index} onClick={() => handleClick(index)}>
+                <h3>{space}</h3>
               </div>
             </>
           );
         })}
-        {/* <p onClick={() => handleClick(0)}> S</p>
-        <p>O</p> <p>X</p>
-        <hr />
-        <p>X</p>
-        <p>O</p> <p>X</p>
-        <hr />
-        <p>X</p> <p>O</p> <p>X</p> */}
       </div>
     </div>
   );
