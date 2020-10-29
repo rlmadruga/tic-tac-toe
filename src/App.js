6ce4d83f384a115d29cb786902b5048f715cf4ec
Rhaysa Madruga <rysamad@gmail.com>
2020-10-28 22:39:11 -0300

    Game done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const initialBoard = Array(9).fill(null);
 
 //print the boar with empty strings - OK
 // check to see if cell is empty - OK
-//when we click to set x or O
+//when we click to set x or O - OK
 //check winning condition
 
 function App() {
   function handleClick(index) {
-    console.log(index, board[index]);
+    // console.log(index, board[index]);
+
+    if (winner) {
+      alert('Acabou o jogo!');
+      return null;
+    }
 
     if (board[index] === null) {
       let newBoard = [...board];
@@ -21,19 +26,60 @@ function App() {
   }
 
   const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
   const [board, setBoard] = useState(initialBoard);
+
+  const checkWinner = () => {
+    const list = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ];
+
+    list.forEach((cells) => {
+      if (cells.every((cell) => cell === 'O')) {
+        setWinner('O');
+      }
+
+      if (cells.every((cell) => cell === 'X')) {
+        setWinner('X');
+      }
+    });
+  };
+
+  const checkDraw = () => {
+    if (board.every((item) => item !== null) && winner === null) {
+      setWinner('D');
+    }
+  };
+
+  checkDraw();
+
+  useEffect(checkWinner, [board]);
+
+  const resetGame = () => {
+    setCurrentPlayer('X');
+    setBoard(initialBoard);
+    setWinner(null);
+  };
 
   return (
     <div className='App'>
       <p>Tic Tac Toe</p>
-      <div className='board'>
+      <div className={`board ${winner ? 'game-over' : ''}`}>
         {board.map((space, index) => {
           return (
             <>
-              {/* {index === 3 || index === 6 ? <br /> : null} */}
               <div
-                className={`cell ${space}`}
                 key={index}
+                className={`cell ${space}`}
                 onClick={() => handleClick(index)}
               >
                 <h3>{space}</h3>
@@ -42,6 +88,20 @@ function App() {
           );
         })}
       </div>
+      {winner && (
+        <footer>
+          {winner === 'D' ? (
+            <h2 className='win-message'>
+              <span className={winner}>Draw!</span>
+            </h2>
+          ) : (
+            <h2 className='win-message'>
+              <span className={winner}>{winner}</span> won!
+            </h2>
+          )}
+          <button onClick={() => resetGame()}>Restart!</button>
+        </footer>
+      )}
     </div>
   );
 }
